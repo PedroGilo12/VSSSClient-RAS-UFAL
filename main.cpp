85d5a9695af0359cf0856b96eb2b5cc084f8b539
PedroGilo12 <phvg@ic.ufal.br>
2025-12-18 22:54:13 -0300

    feature: add simple behaviour tree sample

--- main.cpp
+++ main.cpp
@@ -7,99 +7,113 @@
 #include <clients/actuator/actuatorclient.h>
 #include <clients/replacer/replacerclient.h>
 
-int main(int argc, char *argv[]) {
-    QCoreApplication a(argc, argv);
+#include <iostream>
 
-    // Starting timer
+#include "behaviortree_cpp/bt_factory.h"
+#include "behaviortree_cpp/loggers/groot2_publisher.h"
+
+static bool test = false;
+
+static VisionClient *visionClient;
+static RefereeClient *refereeClient;
+static ReplacerClient *replacerClient;
+static ActuatorClient *actuatorClient;
+
+class IsAtack : public BT::SyncActionNode
+{
+public:
+    IsAtack(const std::string& name)
+        : BT::SyncActionNode(name, {}) {}
+
+    BT::NodeStatus tick() override
+    {
+        std::cout << "IsAtack\n";
+
+        fira_message::sim_to_ref::Environment lastEnv = visionClient->getLastEnvironment();
+        if(lastEnv.has_frame()) {
+            fira_message::Frame lastFrame = lastEnv.frame();
+
+            QString robotDebugStr = QString("Robot %1 -> x: %2 y: %3 ori: %4")
+                        .arg(lastFrame.robots_yellow(0).robot_id())
+                        .arg(lastFrame.robots_yellow(0).x())
+                        .arg(lastFrame.robots_yellow(0).y())
+                        .arg(lastFrame.robots_yellow(0).orientation());
+                std::cout << robotDebugStr.toStdString() + '\n';
+
+            if(lastFrame.robots_yellow(0).x() < 0.0) {
+                return BT::NodeStatus::SUCCESS;
+            }
+        } 
+
+        return BT::NodeStatus::FAILURE;
+    }
+};
+
+class Defense : public BT::SyncActionNode
+{
+public:
+    Defense(const std::string& name)
+        : BT::SyncActionNode(name, {}) {}
+
+    BT::NodeStatus tick() override
+    {
+        std::cout << "Defense\n";
+        actuatorClient->sendCommand(0, 15, 15);
+        return BT::NodeStatus::SUCCESS;
+    }
+};
+
+class Atack : public BT::SyncActionNode
+{
+public:
+    Atack(const std::string& name)
+        : BT::SyncActionNode(name, {}) {}
+
+    BT::NodeStatus tick() override
+    {
+        std::cout << "Atack\n";
+        actuatorClient->sendCommand(0, -15, -15);
+        return BT::NodeStatus::SUCCESS;
+    }
+};
+
+int main(int argc, char *argv[])
+{
+
+    BT::BehaviorTreeFactory factory;
+
+    factory.registerNodeType<IsAtack>("IsAtack");
+    factory.registerNodeType<Defense>("Defense");
+    factory.registerNodeType<Atack>("Atack");
+
+    auto tree = factory.createTreeFromFile("../bht/basic_bht.xml");
+
+    BT::Groot2Publisher publisher(tree);
+
     Timer timer;
 
-    // Creating client pointers
-    VisionClient *visionClient = new VisionClient("224.0.0.1", 10002);
-    RefereeClient *refereeClient = new RefereeClient("224.5.23.2", 10003);
-    ReplacerClient *replacerClient = new ReplacerClient("224.5.23.2", 10004);
-    ActuatorClient *actuatorClient = new ActuatorClient("127.0.0.1", 20011);
+    visionClient = new VisionClient("224.0.0.1", 10002);
+    refereeClient = new RefereeClient("224.5.23.2", 10003);
+    replacerClient = new ReplacerClient("224.5.23.2", 10004);
+    actuatorClient = new ActuatorClient("127.0.0.1", 20011);
 
-    // Setting our color as BLUE at left side
     VSSRef::Color ourColor = VSSRef::Color::YELLOW;
     bool ourSideIsLeft = false;
 
-    // Desired frequency (in hz)
     float freq = 60.0;
 
-    // Setting actuator and replacer teamColor
     actuatorClient->setTeamColor(ourColor);
     replacerClient->setTeamColor(ourColor);
 
-    while(1) {
-        // Start timer
-        timer.start();
 
-        // Running vision and referee clients
+    while (true)
+    {
         visionClient->run();
         refereeClient->run();
 
-        // Debugging vision
-        fira_message::sim_to_ref::Environment lastEnv = visionClient->getLastEnvironment();
-        if(lastEnv.has_frame()) {
-            // Taking last frame
-            fira_message::Frame lastFrame = lastEnv.frame();
-
-            // Debugging ball
-            std::cout << "\n===== BALL =====\n";
-            QString ballDebugStr = QString("Ball x: %1 y: %2")
-                                .arg(lastFrame.ball().x())
-                                .arg(lastFrame.ball().y());
-            std::cout << ballDebugStr.toStdString() + '\n';
-
-            // Debugging blue robots
-            std::cout << "\n===== BLUE TEAM =====\n";
-            for(int i = 0; i < lastFrame.robots_blue_size(); i++) {
-                QString robotDebugStr = QString("Robot %1 -> x: %2 y: %3 ori: %4")
-                        .arg(lastFrame.robots_blue(i).robot_id())
-                        .arg(lastFrame.robots_blue(i).x())
-                        .arg(lastFrame.robots_blue(i).y())
-                        .arg(lastFrame.robots_blue(i).orientation());
-                std::cout << robotDebugStr.toStdString() + '\n';
-            }
-
-            // Debugging yellow robots
-            std::cout << "\n===== YELLOW TEAM =====\n";
-            for(int i = 0; i < lastFrame.robots_yellow_size(); i++) {
-                QString robotDebugStr = QString("Robot %1 -> x: %2 y: %3 ori: %4")
-                        .arg(lastFrame.robots_yellow(i).robot_id())
-                        .arg(lastFrame.robots_yellow(i).x())
-                        .arg(lastFrame.robots_yellow(i).y())
-                        .arg(lastFrame.robots_yellow(i).orientation());
-                std::cout << robotDebugStr.toStdString() + '\n';
-            }
-        }
-
-        // Sending robot commands for robot 0, 1 and 2
-        actuatorClient->sendCommand(0, 0, 0);
-        actuatorClient->sendCommand(1, 0, 0);
-        actuatorClient->sendCommand(2, 0, 0);
-
-        // If is kickoff, send this test frame!
-        if(refereeClient->getLastFoul() == VSSRef::Foul::KICKOFF) {
-            replacerClient->placeRobot(0, ourSideIsLeft ? -0.2 : 0.2, 0, 0);
-            replacerClient->placeRobot(1, ourSideIsLeft ? -0.2 : 0.2, 0.2, 0);
-            replacerClient->placeRobot(2, ourSideIsLeft ? -0.2 : 0.2, -0.2, 0);
-            replacerClient->sendFrame();
-        }
-
-        // Stop timer
-        timer.stop();
-
-        // Sleep for remainingTime
-        long remainingTime = (1000 / freq) - timer.getMiliSeconds();
-        std::this_thread::sleep_for(std::chrono::milliseconds(remainingTime));
+        tree.tickOnce(); 
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
     }
 
-    // Closing clients
-    visionClient->close();
-    refereeClient->close();
-    replacerClient->close();
-    actuatorClient->close();
-
-    return a.exec();
+    return 0;
 }
